d72df73437e5e8aef672b55a81e69238a3705897
unknown <mohamedelfeky700@gmail.com>
2021-08-23 12:27:46 +0200

    add strategy pattern project

---
+++ StrategyPattern/Business/Strategies/SalesTax/SwedenSalesTaxStrategy.cs
@@ -0,0 +1,20 @@
+﻿using Strategy_Pattern_First_Look.Business.Models;
+
+namespace Strategy_Pattern_First_Look.Business.Strategies.SalesTax
+{
+    public class SwedenSalesTaxStrategy : ISalesTaxStrategy
+    {
+        public decimal GetTaxFor(Order order)
+        {
+            var destination = order.ShippingDetails.DestinationCountry.ToLowerInvariant();
+            var totalPrice = order.TotalPrice;
+
+            if (destination == order.ShippingDetails.OriginCountry.ToLowerInvariant())
+            {
+                return totalPrice * 0.25m;
+            }
+
+            return 0;
+        }
+    }
+}
