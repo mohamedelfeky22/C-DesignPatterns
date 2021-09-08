77fb3c7a51bcae14771205c50463b256ccc3409e
unknown <mohamedelfeky700@gmail.com>
2021-09-08 11:53:44 +0200

    add strategy pattern project

---
+++ Singleton Pattern/Singleton/BOs/LoadBalancer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton.BOs
+{
+    public sealed class LoadBalancer
+    {
+        // Lock synchronization object
+        private static readonly LoadBalancer instance = new LoadBalancer();
+        // Note: constructor is 'private'
+        private LoadBalancer()
+        {
+        }
+        public static LoadBalancer GetLoadBalancer()
+        {
+            return instance;
+        }
+
+    }
+}
---
+++ Singleton Pattern/Singleton/Program.cs
@@ -0,0 +1,26 @@
+﻿using Singleton.BOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var b1 = LoadBalancer.GetLoadBalancer();
+            var b2 = LoadBalancer.GetLoadBalancer();
+            var b3 = LoadBalancer.GetLoadBalancer();
+            var b4 = LoadBalancer.GetLoadBalancer();
+            // Confirm these are the same instance
+            if (b1 == b2 && b2 == b3 && b3 == b4)
+            {
+                Console.WriteLine("Same instance\n");
+            }
+            Console.ReadKey();
+        }
+    }
+}
